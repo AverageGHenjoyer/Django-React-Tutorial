89e406ac1582a679ae8687598260025ddbe713a7
maciej-PC <maciej.ko1444@gmail.com>
2024-12-27 13:52:54 +0100

    .

---
+++ frontend/src/api.js
@@ -0,0 +1,22 @@
+// Anywhere we send a request, we will check access token and if we do then axios will automatically add it to the request
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token){
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+export default api
